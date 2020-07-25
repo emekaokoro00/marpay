b8cdf9edfc57e720f505e7b96e5d265b470de3cd
emekaokoro <emeka.okoro.00@gmail.com>
2020-07-24 23:43:24 -0500

    depl

---
+++ nginx/release-tasks.sh
@@ -0,0 +1,5 @@
+# run migrations
+python ./marpay/manage.py makemigrations
+python ./marpay/manage.py migrate
+
+# python manage.py collectstatic --noinput 
--- release-tasks.sh
+++
@@ -1,5 +0,0 @@
-# run migrations
-python marpay/manage.py makemigrations
-python marpay/manage.py migrate
-
-# python manage.py collectstatic --noinput 
