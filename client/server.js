d3a25ce2b2eb5a8940c215cb9b6d37762f7e0c0e
emekaokoro <emeka.okoro.00@gmail.com>
2020-07-27 15:41:17 -0500

    depl

--- client/server.js
+++ client/server.js
@@ -7,5 +7,8 @@ app.use(express.static(__dirname + '/src')); // or /src/website instead of /src
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/src/index.html'));
 });
+
+console.log('listening ' + path.join(__dirname + '/src/index.html'));
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
