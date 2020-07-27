7510e62ce28ba1a41661cfccefd70f719f22840a
emekaokoro <emeka.okoro.00@gmail.com>
2020-07-27 18:56:04 -0500

    depl

--- client/server.js
+++ client/server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the src directory
-app.use(express.static(__dirname)); // or [ + '/src/static'] instead of [ + '/src']
+app.use(express.static(__dirname + '/dist/client')); // or [ + '/src/static'] instead of [ + '/src']
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/client/index.html'));
 });
 
 console.log('listening ' + path.join(__dirname + '/src/index.html'));
@@ -13,4 +13,4 @@ console.log('PORT ' + process.env.PORT);
 
 // Start the app by listening on the default Heroku port
 // app.listen(process.env.PORT || 8080);
-app.listen(process.env.PORT || 8000);
+app.listen(process.env.PORT || 8080);
