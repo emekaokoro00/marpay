43482d870a8b6b53d427a20204a1b2657cdf56cf
emekaokoro <emeka.okoro.00@gmail.com>
2020-07-27 21:55:41 -0500

    depl

---
+++ client/server_bk1.js
@@ -0,0 +1,15 @@
+// Install express server 
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve only the static files form the src directory
+app.use(express.static(__dirname + '/dist/client')); // or [ + '/src/static'] instead of [ + '/src']
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/client/index.html'));
+});
+// console.log('PORT ' + process.env.PORT);
+
+// Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8000);
